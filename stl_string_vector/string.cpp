0eed67f0831e533500f0f53ef38b51ad85bb19d9
tinylamb <642146887@qq.com>
2015-08-14 09:17:02 +0800

    quick commit

--- stl_string_vector/string.cpp
+++ stl_string_vector/string.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 using namespace std;
 
 void string_empty();
+void num_to_string();
 
 int main(){
-    string_empty();
+//    string_empty();
+    num_to_string();
+    return 0;
 }
 
 void string_empty(){
@@ -15,4 +19,16 @@ void string_empty(){
     s = (s1.empty())?s2:s1;
     if (s == "gaoyang")
         cout << s << "==" << "gaoyang" << endl;
+}
+
+void num_to_string(){
+    string s;
+    uint32_t dwInteger;
+    dwInteger = 2134242112;
+    ostringstream ss;
+    ss << dwInteger;
+    s = ss.str();
+    cout << s << endl;
+    //s += 4.5;
+
 }
