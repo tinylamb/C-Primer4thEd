6762e02c490d097e607152d06cc70f838c6b8a23
tinylamb <642146887@qq.com>
2015-08-17 23:59:40 +0800

    quick commit

--- stl_string_vector/string.cpp
+++ stl_string_vector/string.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+//#include <stdlib.h>
+#include <cstdlib>
 using namespace std;
 
 void string_empty();
 void num_to_string();
+void string_add();
+void string_to_uint32();
 
 int main(){
 //    string_empty();
-    num_to_string();
+    string_to_uint32();
     return 0;
 }
 
@@ -30,5 +34,32 @@ void num_to_string(){
     s = ss.str();
     cout << s << endl;
     //s += 4.5;
+
+}
+
+void string_add(){
+    string list = "gaoyangx,1|gaoyang,2";
+    string g = "gaoyang";
+    size_t p = list.find(g + ",");
+    cout << "position : " << p << endl;
+
+}
+
+void string_to_uint32(){
+    string sNum = "n1234name56789";
+    char *t;
+    uint32_t dwNum = strtoul(sNum.c_str(), &t, 10);
+    if(sNum == t){
+        cout << "failed." << endl;
+        cout << "dwNum :" << dwNum; //失败的话dwNum ＝ 0
+    }
+    else{
+        cout << "dwNum : " << dwNum << endl;
+        if((*t) != '\0')
+            cout << "last : " << t << endl;
+    }
+
+    //取值的范围 0 ~ 2^32-1
+    //123name45678 返回123  t指针指向字符n
 
 }
