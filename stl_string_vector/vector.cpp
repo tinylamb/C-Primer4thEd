56009061b44e4f1c73ddb681827a01749fe6cc45
tinylamb <642146887@qq.com>
2015-10-18 09:56:04 +0800

    quick commit

--- stl_string_vector/vector.cpp
+++ stl_string_vector/vector.cpp
@@ -18,17 +18,31 @@
 
 #include <iostream>
 #include <vector>
+#include <string>
 #include <stdexcept>
+#include <stdint.h>
 using namespace std;
 
 void outofrange();
 void vectorrefer(vector<int> &vec);
+void vectorpushback();
+
+class CInfo{
+    public:
+        CInfo():m_sName("lambda"), m_dwAge(25){}
+        CInfo(string sName, uint32_t dwAge){
+            m_sName = sName;
+            m_dwAge = dwAge;
+        }
+    public:
+        string m_sName;
+        uint32_t m_dwAge;
+
+
+};
 
 int main(){
-    vector<int> v(10);
-    vectorrefer(v);
-    cout << "vec value of v(0) : " << v.at(0) << endl;
-    return 0;
+    vectorpushback();
 }
 
 void outofrange(){
@@ -44,4 +58,18 @@ void outofrange(){
 
 void vectorrefer(vector<int> &vec){
     vec.at(0) = 1;
+}
+
+void vectorpushback(){
+    vector<CInfo> vPerson;
+    CInfo oPerson;//如果构造函数没有指定是public 这里会提示调用了private成员
+    for(uint32_t index = 0; index < 10; ++index){
+        oPerson.m_dwAge = index;
+        vPerson.push_back(oPerson);//从输出结果看push_back是值拷贝
+    }
+    vector<CInfo>::const_iterator iter;
+    for(iter = vPerson.begin(); iter != vPerson.end(); ++iter){
+        cout << iter->m_sName << " " << iter->m_dwAge << endl;
+
+    }
 }
